a88787b0ec54dcacc390968d47e022e802205be3
moremode <herobrin3000@gmail.com>
2019-01-14 22:56:51 +0300

    auth add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
 
   def reg
   end
+
+  def auth_post
+    username = params[:username]
+    password = params[:password]
+    if User.find_by_username(username) != nil && User.find_by_username(username).password == password
+      session[:name] = username
+    else
+      flash[:error] = 'Пользователь с таким логином и паролем не найден'
+    end
+    redirect_to root_path
+  end
+
+  def logout
+    session[:name] = ''
+    redirect_to '/auth'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/auth', to: 'users#auth'
   get '/reg', to: 'users#reg'
+  get '/logout', to: 'users#logout'
+
+  post '/auth', to: 'users#auth_post'
 
   root 'users#auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
