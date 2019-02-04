26cd02b4b8b8592be1cb8fafe458d54232a60ffd
moremode <herobrin3000@gmail.com>
2019-02-04 20:51:19 +0300

    valid v.2

--- app/models/debenture.rb
+++ app/models/debenture.rb
@@ -1,4 +1,8 @@
 class Debenture < ApplicationRecord
   belongs_to :passport1, :class_name => "Passport"
   belongs_to :passport2, :class_name => "Passport"
+  validates :count, presence: true
+  validates :date, presence: true
+  validates :last_date, presence: true
+  validates :percent, presence: true
 end
--- app/models/passport.rb
+++ app/models/passport.rb
@@ -6,4 +6,9 @@ class Passport < ApplicationRecord
   validates :number, presence: true
   validates :given, presence: true
   validates :address, presence: true
+  validates :first_name, format: { with: /[А-Яа-я]/, message: "only allows letters" }
+  validates :last_name, format: { with: /[А-Яа-я]/, message: "only allows letters" }
+  validates :middle_name, format: { with: /[А-Яа-я]/, message: "only allows letters" }
+  validates :serial, format: { with: /\d/, message: "only numbers" }, length: { is:4, message: "4 symbols"}
+  validates :number, format: { with: /\d/, message: "only numbers" }, length: { is:6, message: "6 symbols"}
 end
