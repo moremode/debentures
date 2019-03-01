0d37473790169849db21ac2bbc8f890e9f83f58e
Andrew Chizhov <44645568+moremode@users.noreply.github.com>
2019-03-01 14:56:13 +0300

    migrates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.precompile += %w( .svg .eot .woff .ttf .otf)
 end
